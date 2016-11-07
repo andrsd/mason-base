16144e727a15c5802ebac98f8dd1bbada6820247
David Andrs <andrsd@gmail.com>
2016-11-07 08:32:04 -0700

    Adding gcc-6.2.0

---
+++ system/gcc/files/gcc-6.2.0.lua
@@ -0,0 +1,13 @@
+local pkgVer = "6.2.0"
+local pkgName = "gcc-" .. pkgVer
+local packages_dir = os.getenv('PACKAGES_DIR')
+
+prepend_path("PATH",  pathJoin(packages_dir, "gcc", pkgVer, "bin"))
+prepend_path("LIBRARY_PATH",  pathJoin(packages_dir, "gcc", pkgVer, "lib"))
+
+setenv("CC",   "gcc-" .. pkgVer)
+setenv("CXX",  "g++-" .. pkgVer)
+local fc = "gfortran-" .. pkgVer
+setenv("FC",  fc)
+setenv("F90", fc)
+setenv("F77", fc)
