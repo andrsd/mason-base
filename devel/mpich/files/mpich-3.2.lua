3663d22fda53c41d0f46c2e78b79006427ed1c6a
David Andrs <andrsd@gmail.com>
2016-11-07 09:26:54 -0700

    Adding MPICH-3.2

---
+++ devel/mpich/files/mpich-3.2.lua
@@ -0,0 +1,21 @@
+-- MPICH 3.2
+
+whatis("MPICH")
+
+local packages_dir = os.getenv('PACKAGES_DIR')
+local pkgName = "mpich"
+local pkgVer  = "3.2"
+local arch    = os.getenv('ARCH')
+
+prepend_path("PATH",  pathJoin(packages_dir, pkgName, pkgVer, arch, "bin"))
+prepend_path("LIBRARY_PATH", pathJoin(packages_dir, pkgName, pkgVer, arch, "lib"))
+
+pushenv("CC",  "mpicc")
+pushenv("CXX", "mpicxx")
+pushenv("FC",  "mpif90")
+pushenv("F90", "mpif90")
+pushenv("F77", "mpif77")
+
+
+local modulepath_root = os.getenv("MODULEPATH_ROOT")
+prepend_path('MODULEPATH', pathJoin(modulepath_root, "mpi", "llvm-opt", pkgName))
