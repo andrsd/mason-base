2f63e0d36666f2f14c1a676d327403448d1b565b
David Andrs <andrsd@gmail.com>
2020-03-11 12:15:55 -0600

    Adding gfortran/8.2.0, 8.3.0

---
+++ system/gcc/files/gfortran-8.2.0.lua
@@ -0,0 +1,11 @@
+local pkgVer = myModuleVersion()
+local pkgName = "gcc-" .. pkgVer
+local packages_dir = os.getenv('PACKAGES_DIR')
+
+prepend_path("PATH",  pathJoin(packages_dir, "gcc", pkgVer, "bin"))
+prepend_path("LIBRARY_PATH",  pathJoin(packages_dir, "gcc", pkgVer, "lib"))
+
+local fc = "gfortran-" .. pkgVer
+setenv("FC",  fc)
+setenv("F90", fc)
+setenv("F77", fc)
