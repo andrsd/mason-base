d4e4bb58ff3c36c8531357b87c3c3343ac1bc266
David Andrs <andrsd@gmail.com>
2020-03-11 12:13:51 -0600

    Adding gcc/8.2.0

---
+++ system/gcc/files/gcc-8.2.0.lua
@@ -0,0 +1,13 @@
+local pkgVer = myModuleVersion()
+local pkgName = "gcc-" .. pkgVer
+local packages_dir = os.getenv('PACKAGES_DIR')
+
+prepend_path("PATH",  pathJoin(packages_dir, "gcc", pkgVer, "bin"))
+prepend_path("LIBRARY_PATH",  pathJoin(packages_dir, "gcc", pkgVer, "lib"))
+
+setenv("CC",   "gcc-" .. pkgVer)
+setenv("CXX",  "g++-" .. pkgVer)
+local fc = "gfortran-" .. pkgVer
+setenv("FC",  fc)
+setenv("F90", fc)
+setenv("F77", fc)
