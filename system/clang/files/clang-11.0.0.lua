0b67a73b45c839e43e5d988302498d623dcbd68a
David Andrs <andrsd@gmail.com>
2021-01-20 10:09:12 -0700

    Adding clang-11.0.0

---
+++ system/clang/files/clang-11.0.0.lua
@@ -0,0 +1,15 @@
+local pkgVer = myModuleVersion()
+local packages_dir = os.getenv('PACKAGES_DIR')
+
+prepend_path("PATH", pathJoin(packages_dir, "clang", pkgVer, "bin"))
+prepend_path("LIBRARY_PATH", pathJoin(packages_dir, "clang", pkgVer, "lib"))
+prepend_path("LIBRARY_PATH", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib")
+
+pushenv("CC",   "clang")
+pushenv("CXX",  "clang++")
+pushenv("LLVM", pathJoin(packages_dir, "clang", pkgVer, "bin"))
+
+prepend_path("C_INCLUDE_PATH", "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include")
+
+prepend_path("CPLUS_INCLUDE_PATH", "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include")
+prepend_path("CPLUS_INCLUDE_PATH", pathJoin(packages_dir, "clang", pkgVer, "include", "c++", "v1"))
